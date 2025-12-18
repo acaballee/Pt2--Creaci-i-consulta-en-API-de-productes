79ca8dce5053fbd95674f1ed95fa9cf9544da129
«acaballee» <alexcaballe@iesebre.com>
2025-12-18 16:25:02 +0100

    Estructura inicial de fitchers

---
+++ lib/models/product.dart
@@ -0,0 +1,26 @@
+class Product {
+  final int id;
+  final String name;
+  final String description;
+  final double price;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      description: json['description'] as String,
+      price: (json['price'] as num).toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'name': name, 'description': description, 'price': price};
+  }
+}
